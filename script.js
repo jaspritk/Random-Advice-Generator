1e24f4f54100de93c1e9533bc25c3c4ee3b19e22
jaspritk <jaspritkaur2920@gmail.com>
2022-04-22 14:43:41 +0530

    first commit

--- script.js
+++ script.js
@@ -2,16 +2,22 @@ const adviceId = document.getElementById("adviceId");
 const adviceText = document.getElementById("adviceText");
 const btn = document.getElementById("btn");
 
-fetch('https://api.adviceslip.com/advice').then(response => {
-    return response.json();
-}).then(data => {
-    // console.log(data); 
-    const adviceObject = data.slip;
-    console.log(adviceObject);
-    console.log(adviceObject.id);
-    console.log(adviceObject.advice);
-    adviceId.innerHTML = `<p>Advice #${adviceObject.id}</p>`
-    adviceText.innerHTML = `<p>"${adviceObject.advice}"</p>`
-}).catch(error => {
-    console.log(error);
-})
+function newData() {
+
+    btn.disabled = true;
+    setTimeout(() => {
+        fetch('https://api.adviceslip.com/advice').then(response => {
+            return response.json();
+        }).then(data => {
+            const adviceObject = data.slip;
+            console.log(adviceObject);
+            console.log(adviceObject.id);
+            console.log(adviceObject.advice);
+            adviceId.innerHTML = `<p>Advice #${adviceObject.id}</p>`
+            adviceText.innerHTML = `<p>"${adviceObject.advice}"</p>`
+        }).catch(error => {
+            console.log(error);
+        })
+        btn.disabled = false;
+    }, 500)
+}
